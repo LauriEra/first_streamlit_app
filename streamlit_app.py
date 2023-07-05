3590902e1263b28f1802d5eeb3d8f2484bb094e6
Lauri Era <era.lauri@gmail.com>
2023-07-05 17:24:43 +0300

    Update streamlit_app.py
    AS json

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,5 @@ streamlit.dataframe(fruits_to_show)
 
 # Fruityvice
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
